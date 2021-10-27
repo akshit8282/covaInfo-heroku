130a49f753c6c276d07570846151a5808c1aabef
akshit8282 <akshitbatra222@gmail.com>
2021-10-27 12:07:18 +0530

    modified

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const morgan=require('morgan');
+
 const mongoose=require('mongoose');
 const bodyparser=require('body-parser');
 const cors=require('cors');
@@ -12,12 +12,12 @@ app.use(cors());
 
 app.use(express.urlencoded({limit: '50mb',extended:false}));
 app.use(express.json({limit: '50mb'}));
-
+app.use(bodyparser.json())
 app.use('/api/videos/',express.static('uploads'))
 
 
 //setting mongoose
-mongoose.connect("mongodb+srv://akshit:batra@cluster0.anjms.mongodb.net/CovaInfoHeroku?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://akshit:batra@cluster0.anjms.mongodb.net/covaInfoHeroku?retryWrites=true&w=majority", {
   useNewUrlParser: "true",
   useUnifiedTopology: 'true',
   useCreateIndex: 'true'
@@ -38,6 +38,6 @@ app.use('/api/upload',checkAuth,require('./Routes/upload'));
 console.log(__dirname);
 //app.use('/api/videos',require('./Routes/video'));
 app.use('/api/videoList',require('./Routes/videoList'));
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log('app is running on 3000');
 })
